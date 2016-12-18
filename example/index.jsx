8b90ae50630cad688176beb5f6c276e4eba37889
TimTheSinner <timthesinner@gmail.com>
2016-12-17 22:20:48 -0700

    First commit

---
+++ example/index.jsx
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2016 TimTheSinner All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * @author TimTheSinner
+ */
+import React from 'react';
+import { render } from 'react-dom';
+import * as D3 from 'd3';
+
+import VirtualDOM from '../src';
+
+document.body.innerHTML = '<div id="body" />';
+render (
+  (<div>Test</div>), document.getElementById('body')
+);
+
+if (module.hot) {
+  module.hot.accept();
+}
